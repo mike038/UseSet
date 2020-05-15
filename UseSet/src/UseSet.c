71d75544d7bb9dc58168f98bd275b43aac20ec41
Miguel Aguilar <miguelaguilar038@gmail.com>
2020-05-15 08:24:50 -0500

    Pantalla usuario

--- UseSet/src/UseSet.c
+++ UseSet/src/UseSet.c
@@ -1,53 +1,81 @@
 /*
- ============================================================================
- Name        : UseSet.c
- Author      : Miguel Aguilar
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
+ * UseSet.c
+ *
+ *  Created on: May 14, 2020
+ *      Author: Miguel Aguilar
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "..\..\..\Set\Set\Set.h"
+#include "..\..\Set\Set.h"
 
 #define TAM 100
 
 int main(void) {
 
 	int numbers[TAM];
-	int n;
-	setbuf(stdout,NULL);
+	int n, opcion = 0;
+	setbuf(stdout, NULL);
 
 	Set bst = set_create(compareInts); //binary search tree
 
-	for (int i=0; i<TAM; i++)
-	{
-		printf("Dame dato (negativo para salir): ");
-		scanf("%d",&n);
-		if (n<0) break;
-		numbers[i] = n;
-		set_add(bst,&numbers[i]);
+	while (opcion != 8) {
+		printf("\nElige una opcion: \n");
+		printf("1. Agregar datos.\n");
+		printf("2. Buscar un dato\n");
+		printf("3. Eliminar un dato\n");
+		printf("4. Imprimir en formato Pre-Orden\n");
+		printf("5. Imprimir en formato In-Orden\n");
+		printf("6. Imprimir en formato Post-Orden\n");
+		printf("7. Eliminar arbol\n");
+		printf("8. Salir\n");
+		scanf("%d", &opcion);
+
+		switch (opcion) {
+		case 1:
+			for (int i = 0; i < TAM; i++) {
+				printf("Dame dato (negativo para salir): ");
+				scanf("%d", &n);
+				if (n < 0)
+					break;
+				numbers[i] = n;
+				set_add(bst, &numbers[i]);
+			}
+			break;
+		case 2:
+			printf("Dato a buscar: ");
+			scanf("%d", &n);
+			if (set_containsValue((Type) &n, bst))
+				printf("Valor encontrado\n");
+			else
+				printf("Valor no encontrado\n");
+			break;
+		case 3:
+			printf("Dato a eliminar: ");
+			scanf("%d", &n);
+			if (set_removeValue((Type) &n, bst))
+				printf("Valor eliminado\n");
+			else
+				printf("Valor no encontrado\n");
+			break;
+		case 4:
+			printf("\n햞bol en Pre-orden\n");
+			set_printPreOrder(bst);
+			break;
+		case 5:
+			printf("\n햞bol en In-orden\n");
+			set_printInOrder(bst);
+			break;
+		case 6:
+			printf("\n햞bol en Post-orden\n");
+			set_printPostOrder(bst);
+			break;
+		case 7:
+
+			break;
+		}
 
 	}
-
-	printf("Dato a eliminar: ");
-	scanf("%d",&n);
-	if (set_removeValue((Type)&n,bst))
-		printf("Valor eliminado\n");
-	else
-		printf("Valor no encontrado\n");
-
-	printf("Dato a buscar: ");
-		scanf("%d",&n);
-		if (set_containsValue((Type)&n,bst))
-			printf("Valor encontrado\n");
-		else
-			printf("Valor no encontrado\n");
-
-	printf("\n햞bol en preorden\n");
-	set_printPreOrder(bst);
 
 	return 0;
 }
